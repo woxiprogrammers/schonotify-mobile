658590f3450df5683e56e01d1cb3482a2bfe77d5
Bharat Makwana <bharat.woxi@gmail.com>
2015-10-16 16:03:01 +0530

    Revert "Dashboard login"

--- veza/www/js/app.js
+++ veza/www/js/app.js
@@ -3,58 +3,17 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material', 'ionMdInput'])
+angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleDefault();
-        }
-    });
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
 })
-
-.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
-
-    // Turn off caching for demo simplicity's sake
-    $ionicConfigProvider.views.maxCache(0);
-
-    /*
-    // Turn off back button text
-    $ionicConfigProvider.backButton.previousTitleText(false);
-    */
-
-    $stateProvider
-
-    .state('login', {
-        url: '/login',
-        templateUrl: 'templates/login.html',
-        controller: 'LoginCtrl'
-    })
-
-	.state('app', {
-        url: '/app',
-        templateUrl: 'templates/menu.html',
-        abstract: true,        
-        controller: 'AppCtrl'
-    })    
-
-    .state('app.dashboard', {
-        url: '/dashboard',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/dashboard.html',
-                controller: 'DashboardCtrl'
-            }
-        }
-    });
-
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/login');
-});
